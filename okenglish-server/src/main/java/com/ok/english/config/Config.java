162db84693d145e9467fcaf7cbd1d3f5668a81a6
kalicatmix <2868607141kalicat@gmail.com>
2020-08-02 20:25:43 +0800

    fix smtp lack

--- okenglish-server/src/main/java/com/ok/english/config/Config.java
+++ okenglish-server/src/main/java/com/ok/english/config/Config.java
@@ -6,10 +6,10 @@ public class Config {
     public static final String kafka_topic_log = "log";
     public static final String admin_session_tag = "adminLogin";
     public static final String admin_session_key = "okenglish";
-    public static final String mail_auth_code = "joapvswjroywdaji";
-    public static final String mail_auth_user = "3588164051@qq.com";
-    public static final String mail_smtp_sever = "smtp.qq.com";
-    public static final int mail_smtp_port = 25;
+    public static final String mail_auth_code = "email auth code";
+    public static final String mail_auth_user = "your email";
+    public static final String mail_smtp_sever = "your email server";
+    public static final int mail_smtp_port = "email port";
     public static final String ip_look_server = "http://ip-api.com/json/";
     public static final String ip_look_server_param = "lang=zh-CN";
 }
